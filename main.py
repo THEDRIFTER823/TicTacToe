7ab4e3debaa3e707cc5a929ed2b456517de1e920
THEDRIFTER823 <kadenlee007@gmail.com>
2024-09-05 08:33:01 -0500

    weeeeee

--- main.py
+++ main.py
@@ -153,6 +153,8 @@ def checkifwon():
         winner = True
         messagebox.showinfo("Tic Tac Toe", "O Wins!")
         disable_all_buttons()
+    if count == 9 and winner == False:
+        messagebox.showinfo("TicTacToe", "Tie.")
 
 #Button clicked functions
 def b_click(b):
@@ -170,31 +172,45 @@ def b_click(b):
         checkifwon()
     else:
         messagebox.showerror("Tic Tac Toe", "Hey! That box has already been selected!\nPick another box!")
+#reset
+def reset():
+    global b1, b2, b3, b4, b5, b6, b7, b8, b9
+    global clicked, count
+    clicked = True
+    count = 0
+    #Buttons or something
+    b1 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b1))
+    b2 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b2))
+    b3 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b3))
 
-#Buttons or something
-b1 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b1))
-b2 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b2))
-b3 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b3))
+    b4 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b4))
+    b5 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b5))
+    b6 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b6))
 
-b4 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b4))
-b5 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b5))
-b6 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b6))
+    b7 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b7))
+    b8 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b8))
+    b9 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b9))
 
-b7 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b7))
-b8 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b8))
-b9 = Button(root, text = " ", font = ("Helvetica", 20), height = 3, width = 6, bg = "SystemButtonFace", command = lambda: b_click(b9))
+    #grid
+    b1.grid(row = 0, column = 0)
+    b2.grid(row = 0, column = 1)
+    b3.grid(row = 0, column = 2)
 
-#grid
-b1.grid(row = 0, column = 0)
-b2.grid(row = 0, column = 1)
-b3.grid(row = 0, column = 2)
+    b4.grid(row = 1, column = 0)
+    b5.grid(row = 1, column = 1)
+    b6.grid(row = 1, column = 2)
 
-b4.grid(row = 1, column = 0)
-b5.grid(row = 1, column = 1)
-b6.grid(row = 1, column = 2)
+    b7.grid(row = 2, column = 0)
+    b8.grid(row = 2, column = 1)
+    b9.grid(row = 2, column = 2)
 
-b7.grid(row = 2, column = 0)
-b8.grid(row = 2, column = 1)
-b9.grid(row = 2, column = 2)
+#menu
+my_menu = Menu(root)
+root.config(menu = my_menu)
 
+#options menu
+options_menu = Menu(my_menu, tearoff=False)
+my_menu.add_cascade(label="options", menu=options_menu)
+options_menu.add_command(label="Reset Game", command=reset)
+reset()
 root.mainloop()
